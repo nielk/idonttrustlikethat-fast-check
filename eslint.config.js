a69798a52b90a689955a8d0c7ed849b6ddef3bed
Alexandre Oger <alexandre.oger@fabernovel.ey.com>
2024-06-25 09:24:47 +0200

    update deps + migrating to eslint 9 + better support for esm + comonjs module

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    plugins: {},
+    files: ['src/**/*.js'],
+    ignores: ['**/*.config.js'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+    languageOptions: {},
+  },
+  prettier,
+);
